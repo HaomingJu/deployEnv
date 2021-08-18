098497f30b40fd26941819f44b7e399f205c1fd7
juhaoming@outlook.com <juhaoming@trunk.tech>
2021-08-18 22:52:33 +0800

    Add terminal cfg

--- deployBase.sh
+++ deployBase.sh
@@ -5,7 +5,7 @@ GIT_NAME=juhaoming@outlook.com
 
 
 # base deb
-sudo apt install -y zsh git tig tree silversearcher-ag htop ssh ctags clang-format-8 python2.7-dev python2.7-dbg curl libssl-dev wget
+sudo apt install -y git tig tree silversearcher-ag htop ssh ctags clang-format-8 python2.7-dev python2.7-dbg curl libssl-dev wget
 sudo update-alternatives --install /usr/bin/clang-format clang-format `which clang-format-8` 100
 
 # ssh-keygen
@@ -20,17 +20,3 @@ git config --global user.name ${GIT_NAME}
 
 git clone --depth 1 https://github.com/HaomingJu/Snippets.git ~/.config/haoming/Snippets
 git clone --depth 1 https://github.com/HaomingJu/Cheats.git ~/.config/haoming/Cheats
-
-# navi
-bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
-
-# autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-# deploy rc file
-rm ~/.zshrc && ln -s `pwd`/conf/zshrc ~/.zshrc
